78519efaede12c0bade4c0fe75668335ae69cf0a
Rannie Peralta <rannieperalta092796@gmail.com>
2019-10-31 18:53:27 +0800

    Add tests for highlighting positioned element

---
+++ cypress/integration/highlight--positioned-element.ts
@@ -0,0 +1,51 @@
+import UIGuide from '../../src/ui-guide';
+
+describe('highlight - Positioned Element', () => {
+  let uiguide: typeof UIGuide;
+
+  before(() => {
+    cy.visit('/highlight--positioned-element.html').then((contentWindow) => {
+      uiguide = contentWindow.__LIBRARY__['ui-guide'].default;
+    });
+  });
+
+  it('should not add a "uig-markers-non-positioned" attribute to target element when it has a "position: relative" style', () => {
+    const promise = uiguide.highlight('[data-testid="target-1"]');
+
+    cy.get('[uig-elements-backdrop][uig-markers-show]')
+      .then(() => promise)
+      .then(({ element }) => {
+        expect(element).not.to.have.attr('uig-markers-non-positioned');
+      });
+  });
+
+  it('should not add a "uig-markers-non-positioned" attribute to target element when it has a "position: absolute" style', () => {
+    const promise = uiguide.highlight('[data-testid="target-2"]');
+
+    cy.get('[uig-elements-backdrop][uig-markers-show]')
+      .then(() => promise)
+      .then(({ element }) => {
+        expect(element).not.to.have.attr('uig-markers-non-positioned');
+      });
+  });
+
+  it('should not add a "uig-markers-non-positioned" attribute to target element when it has a "position: fixed" style', () => {
+    const promise = uiguide.highlight('[data-testid="target-3"]');
+
+    cy.get('[uig-elements-backdrop][uig-markers-show]')
+      .then(() => promise)
+      .then(({ element }) => {
+        expect(element).not.to.have.attr('uig-markers-non-positioned');
+      });
+  });
+
+  it('should not add a "uig-markers-non-positioned" attribute to target element when it has a "position: sticky" style', () => {
+    const promise = uiguide.highlight('[data-testid="target-4"]');
+
+    cy.get('[uig-elements-backdrop][uig-markers-show]')
+      .then(() => promise)
+      .then(({ element }) => {
+        expect(element).not.to.have.attr('uig-markers-non-positioned');
+      });
+  });
+});
--- src/utils/is-element-positioned.ts
+++ src/utils/is-element-positioned.ts
@@ -1,25 +1,20 @@
 export default function isElementPositioned(element: Element) {
   // 1st check.
+  // Using the element's computed style position.
+
+  if (getComputedStyle(element).position !== 'static') {
+    return true;
+  }
+
+  // 2nd check.
   // Using the element's first child.
 
   if (element.firstElementChild instanceof HTMLElement) {
     return element.firstElementChild.offsetParent === element;
   }
 
-  // 2nd check.
-  // Using the element's computed style position.
-
-  const style = getComputedStyle(element);
-  if (
-    style.position === 'absolute' ||
-    style.position === 'fixed' ||
-    style.position === 'relative'
-  ) {
-    return true;
-  }
-
   // 3rd check.
-  // Mimicking the 1st check by appending a temporary element.
+  // Mimicking the 2nd check by appending a temporary element.
 
   const tempElement = document.createElement('span');
   tempElement.style.visibility = 'hidden';
