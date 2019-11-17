d401802e4da9bc9770b9b2a072c3d182ffdbfdd7
Rannie Peralta <rannieperalta092796@gmail.com>
2019-11-17 16:05:37 +0800

    Fix tslint error

--- cypress/typings/globals.d.ts
+++ cypress/typings/globals.d.ts
@@ -1,6 +1,7 @@
 import * as _UIGuide from '../../src/ui-guide';
 
 declare global {
+  // tslint:disable-next-line: interface-name
   interface Window {
     __LIBRARY__: {
       'ui-guide': { default: typeof _UIGuide.default };
